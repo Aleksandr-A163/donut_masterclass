0372d5eb30232bdebe1327fb8ac797500ae00db0
Aleksandr-A163 <quark_31@mail.ru>
2021-05-24 00:30:49 +0400

    Update read-more.js

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -1,15 +1,16 @@
-function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-
-    if (dots.style.display === "none") {
-        dots.style.display = "inline";
-        btnText.innerHTML = "Read more";
-        moreText.style.display = "none";
-    } else {
-        dots.style.display = "none";
-        btnText.innerHTML = "less";
-        moreText.style.display = "inline";
-    }
+const btnText = document.getElementById('myBtn');
+const moreText = document.getElementById('more');
+btnText.addEventListener('click', openText);
+let isOpen = false;
+function openText() {
+  console.log(moreText);
+  if (isOpen === false) {
+    moreText.style.display = 'inline';
+    btnText.innerHTML = 'Less';
+    isOpen = true;
+  } else {
+    btnText.innerHTML = 'Read more';
+    moreText.style.display = 'none';
+    isOpen = false;
+  }
 }
