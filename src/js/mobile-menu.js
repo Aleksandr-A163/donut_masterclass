05e7997e40cf8991b7e0751f2b88e00e86982e59
1mishanya107 <1mishanya107@bk.ru>
2021-05-24 16:59:33 +0400

    готовое меню

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,22 +1,6 @@
-// (() => {
-//   const menuBtnRef = document.querySelector("[data-menu-button]");
-//   const mobileMenuRef = document.querySelector("[data-menu]");
-
-//   menuBtnRef.addEventListener("click", () => {
-//     const expanded =
-//       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-//     menuBtnRef.classList.toggle("is-open");
-//     menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-//     mobileMenuRef.classList.toggle("is-open");
-//   });
-// })();
-
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-  const menuLinkRef = document.querySelector("[data-menu-link]");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -26,55 +10,17 @@
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
-
-    menulinkRef.addEventListener("click", () => {
-      const expended =
-      menuBtnRef.getAttribute("aria-expanded") === true || "false";
-      menuBtnRef.classList.toggle("is-open");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-    mobileMenuRef.classList.toggle("is-open");
-
-    });
   });
 
-
+  const links = document.querySelectorAll('.mobile-menu__link');
+const menu = document.querySelector('.mobile-menu');
+const menuBtn = document.querySelector('.menu-button');
+links.forEach(link =>
+  link.addEventListener('click', () => {
+    if (menu.classList.contains('is-open')) {
+      menu.classList.remove('is-open');
+      menuBtn.classList.remove('is-open');
+    }
+  }),
+);
 })();
-
-  // menulinkRef.addEventListener("click", () => {
-  //     menuBtnRef.getAttribute("aria-expanded") === true || "false";
-
-  //   menuBtnRef.classList.toggle("is-open");
-  //   menuBtnRef.setAttribute("aria-expanded");
-
-  //   mobileMenuRef.classList.toggle("is-open");
-
-
-    
-  // });
-
-
-// menuBtnRef.classList.toggle("is-open");
-// menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-// mobileMenuRef.classList.toggle("is-open");
-
-// menuBtnRef.classList.toggle("is-open");
-// menuBtnRef.classList.toggle("is-open");
-// menuBtnRef.setAttribute("aria-expanded");
-
-
-// mobileMenuRef.classList.toggle("is-open");
-  
-//    menuBtnRef.getAttribute("aria-expanded") === true || "false";
-
-// // menuBtnRef.classList.toggle("is-open");
-// menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-
-  // menuBtnRef.getAttribute("aria-expanded") === true || "false";
-
-// menuBtnRef.classList.toggle("is-open");
-// menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-// mobileMenuRef.classList.toggle("is-open");
